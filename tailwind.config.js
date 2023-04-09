d1a7216a07d9bdc998c437154c6e39cf22025bef
lindahandayani25 <lindahandayanismakp2ciparay@gmail.com>
2023-04-09 22:16:00 +0700

    portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#f59e0b',
+        secondary: '#94a3b8',
+        dark: '#020617',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
